c31c53be1d393a37f3beb5ed4a2b349b65999cc8
Kelsey Mott <kelseymott@gmail.com>
2021-12-09 22:47:25 -0500

    the trickle of a distant fountain

--- README.md
+++ README.md
@@ -15,7 +15,7 @@
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
-![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%86/yellow)](src/day05)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
@@ -85,7 +85,7 @@ Both parts: 1.62ms
 Day 03
 Time part 1: 2.191ms
 Time part 2: 4.969ms
-Both parts: 7.16ms
+Both parts: 7.15976ms
 ```
 
 ```
@@ -97,9 +97,9 @@ Both parts: 4.595427ms
 
 ```
 Day 05
-Time part 1: -
+Time part 1: 49.261ms
 Time part 2: -
-Both parts: -
+Both parts: 49.261304ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 8/50
-Total time: 8.335ms
+Total stars: 9/50
+Total time: 64.756ms
 ```
 
 <!--/RESULTS-->
--- src/day05/index.ts
+++ src/day05/index.ts
@@ -1,15 +1,64 @@
 import run from "aocrunner"
 
+interface Point {
+  x: number
+  y: number
+}
+
+interface Line {
+  start: Point
+  end: Point
+}
+
 const parseInput = (rawInput: string) => {
-  return rawInput
+  const lines: Line[] = []
+  rawInput.split(/\r?\n/).map(rawLine => {
+    const startEnd = rawLine.split(' -> ')
+    const start = startEnd[0].split(',')
+    const end = startEnd[1].split(',')
+    const startPoint = { x: Number(start[0]), y: Number(start[1]) }
+    const endPoint = { x: Number(end[0]), y: Number(end[1]) }
+    const line = { start: startPoint, end: endPoint }
+    if (startPoint.x === endPoint.x || startPoint.y === endPoint.y) {
+      lines.push(line)
+    }
+  })
+  return lines
 }
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput)
-  // filter out diagonal lines 
-  // plot lines as number values in a grid
-  // count cells with value > 1
-  return 'norp'
+  const field: (number |  undefined)[][] = []
+  const vertical: Line[] = []
+  const horizontal: Line[] = []
+  input.forEach(line => {
+    if (line.start.x === line.end.x) {
+      vertical.push(line)
+    } else if (line.start.y === line.end.y){
+      horizontal.push(line)
+    }
+  })
+  vertical.forEach(line => {
+    const start = Math.min(line.start.y, line.end.y)
+    const end = Math.max(line.start.y, line.end.y)
+    for(let i = start; i <= end; i++) {
+      if (field[i] === undefined) {
+        field[i] = []
+      }
+      field[i][line.start.x] = (field[i][line.start.x] || 0) + 1
+    }
+  })
+  horizontal.forEach(line => {
+    const start = Math.min(line.start.x, line.end.x)
+    const end = Math.max(line.start.x, line.end.x)
+    if (field[line.start.y] === undefined) {
+      field[line.start.y] = []
+    }
+    for(let i = start; i <= end; i++) {
+      field[line.start.y][i] = (field[line.start.y][i] || 0) + 1
+    }
+  })
+  return field.flat().reduce((p, c) => c !== undefined && c > 1 ? (p || 0) + 1 : p, 0)
 }
 
 const part2 = (rawInput: string) => {
@@ -50,5 +99,5 @@ run({
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 })
