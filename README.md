5ab1e1f5d0a58de1e1023ef0e545736f2d954a77
Kelsey Mott <kelseymott@gmail.com>
2021-12-15 00:07:51 -0500

    part 7ish

--- README.md
+++ README.md
@@ -17,7 +17,7 @@
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
-![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%86/yellow)](src/day07)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
@@ -104,16 +104,16 @@ Both parts: 115.82578699999999ms
 
 ```
 Day 06
-Time part 1: 0.226ms
-Time part 2: 1.368ms
-Both parts: 1.594038ms
+Time part 1: 0.167ms
+Time part 2: 0.903ms
+Both parts: 1.069472ms
 ```
 
 ```
 Day 07
-Time part 1: -
+Time part 1: 8.335ms
 Time part 2: -
-Both parts: -
+Both parts: 8.335247ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 12/50
-Total time: 132.915ms
+Total stars: 13/50
+Total time: 140.726ms
 ```
 
 <!--/RESULTS-->
--- src/day07/index.ts
+++ src/day07/index.ts
@@ -2,18 +2,32 @@ import run from "aocrunner"
 
 const parseInput = (rawInput: string) => rawInput.split(',').map(n => Number(n))
 
+const costOfMovingTo = (input: number[], value: number) => {
+  return input.reduce((p, c) => p + Math.abs(c - value), 0)
+}
+
+const newCostOfMovingTo = (input: number[], value: number) => {
+  return input.reduce((p, c) => {
+    const d = Math.abs(c - value)
+    return p + (d * (d + 1) / 2)
+  }, 0)
+}
+
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput)
-  const lookup: Record<number, number> = {}
-  input.forEach(n => lookup[n] = (lookup[n] || 0) + 1)
-  const uh = Number(Object.entries(lookup).reduce((p, c) => (c?.[1] > p?.[1]) ? c : p)[0])
-  return input.reduce((p, c) => p + Math.abs(c - uh), 0)
+  const cost = Array.from(new Set(input)).reduce((p, c) => {
+    return Math.min(p, costOfMovingTo(input, c))
+  }, Number.MAX_VALUE)
+  return cost
 }
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput)
-
-  return
+  console.log('cost', newCostOfMovingTo(input, 2))
+  const cost = Array.from(new Set(input)).reduce((p, c) => {
+    return Math.min(p, newCostOfMovingTo(input, c))
+  }, Number.MAX_VALUE)
+  return cost
 }
 const testInput = '16,1,2,0,4,2,7,1,2,14'
 run({
@@ -25,10 +39,10 @@ run({
   },
   part2: {
     tests: [
-      // { input: ``, expected: "" },
+      { input: testInput, expected: 168 },
     ],
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: false,
+  onlyTests: true,
 })
