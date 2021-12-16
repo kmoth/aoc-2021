45774751d0d8fe2986efbc19d8603abe021e909e
Kelsey Mott <kelseymott@gmail.com>
2021-12-15 23:18:54 -0500

    reading comprehension ftw

--- README.md
+++ README.md
@@ -17,7 +17,7 @@
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
-[![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%86/yellow)](src/day07)
+[![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
@@ -111,9 +111,9 @@ Both parts: 1.069472ms
 
 ```
 Day 07
-Time part 1: 8.335ms
-Time part 2: -
-Both parts: 8.335247ms
+Time part 1: 25.642ms
+Time part 2: 25.427ms
+Both parts: 51.069561ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 13/50
-Total time: 140.726ms
+Total stars: 14/50
+Total time: 183.46ms
 ```
 
 <!--/RESULTS-->
--- src/day07/index.ts
+++ src/day07/index.ts
@@ -1,35 +1,57 @@
 import run from "aocrunner"
 
+interface Range {
+  min: number
+  max: number
+}
+
 const parseInput = (rawInput: string) => rawInput.split(',').map(n => Number(n))
 
-const costOfMovingTo = (input: number[], value: number) => {
+const part1CostOfMoving = (input: number[], value: number) => {
   return input.reduce((p, c) => p + Math.abs(c - value), 0)
 }
 
-const newCostOfMovingTo = (input: number[], value: number) => {
+const part2CostOfMoving = (input: number[], value: number) => {
   return input.reduce((p, c) => {
     const d = Math.abs(c - value)
     return p + (d * (d + 1) / 2)
   }, 0)
 }
 
+const getRange = (s: Set<number>): Range => {
+  let min = Number.MAX_VALUE
+  let max = Number.MIN_VALUE
+  const d = s.forEach(x => {
+    min = Math.min(x, min)
+    max = Math.max(x, max)
+  })
+  return { min, max }
+}
+
+const getLowestCost = (input: number[], range: Range, costCalculation: (input: number[], value: number) => number): number => {
+  let leastCost = Number.MAX_VALUE
+  for(let i = range.min; i <= range.max; i++) {
+    leastCost = Math.min(costCalculation(input, i), leastCost)
+  }
+  return leastCost
+}
+
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput)
-  const cost = Array.from(new Set(input)).reduce((p, c) => {
-    return Math.min(p, costOfMovingTo(input, c))
-  }, Number.MAX_VALUE)
-  return cost
+  const uniqueSet = new Set(input)
+  const range = getRange(uniqueSet)
+  return getLowestCost(input, range, part1CostOfMoving)
 }
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput)
-  console.log('cost', newCostOfMovingTo(input, 2))
-  const cost = Array.from(new Set(input)).reduce((p, c) => {
-    return Math.min(p, newCostOfMovingTo(input, c))
-  }, Number.MAX_VALUE)
-  return cost
+  const uniqueSet = new Set(input)
+  const range = getRange(uniqueSet)
+  return getLowestCost(input, range, part2CostOfMoving)
 }
+
 const testInput = '16,1,2,0,4,2,7,1,2,14'
+
 run({
   part1: {
     tests: [
@@ -44,5 +66,5 @@ run({
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 })
